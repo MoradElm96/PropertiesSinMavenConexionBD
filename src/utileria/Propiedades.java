28908dbcae90850bc8fc3534575370d64037abb7
Morad elm <78969860+MoradElm96@users.noreply.github.com>
2023-01-27 17:47:07 +0100

    initial commit

---
+++ src/utileria/Propiedades.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utileria;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ *
+ * @author Morad
+ */
+public class Propiedades {
+    //metodo para cargar el archivo properties
+    public Properties cargarArchivoProperties() throws FileNotFoundException, IOException{
+        Properties propiedades = new Properties();//creamos el archivo properties
+        // propiedades.setProperty("conexion.password", "");
+         //ruta absoluta o relativa, en este caso le pasamos la ruta absoluta
+        InputStream file =  getClass().getClassLoader().getResourceAsStream("resources/conexionBD.properties");
+       
+        propiedades.load(file);
+             
+        return propiedades;
+    }
+    
+    
+    
+}
